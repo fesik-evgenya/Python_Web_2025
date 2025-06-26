2f5f075e845afbf8dd52c2ba4c49c7d57e3644d3
Фесик на учёбе <ganef85@mail.ru>
2025-06-26 14:38:05 +0300

    Keyword "in"

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-# match - case (3.10>)
+# ключевое слово in
 
-print('Возможные ходы:\n\tL - влево\n\tR - вправо\n\tF - прямо\n\tQ - выход')
+word = 'поток'
 
-while True:
-    ch = input('Ваш выбор: ')
-    match ch:
-        case 'L' | 'l' | 'Л' | 'л':
-            print('Свернули налево')
-        case 'R' | 'r' | 'Р' | 'р':
-            print('Свернули направо')
-        case 'F' | 'f' | 'Ф' | 'ф':
-            print('Пошли прямо')
-        case 'Q' | 'q' | 'Й' | 'й':
-            print('До свидания!')
-            break
-        case _: #gefault
-            print('Выбор не ясен')
+if 'ток' in word:
+    print('Есть')
