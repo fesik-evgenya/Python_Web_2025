397bb743f3215cf04b7baa764e54ce3d42a9929f
Фесик на учёбе <ganef85@mail.ru>
2025-06-26 15:44:26 +0300

    min, max, average, summ, production + Factorial

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# Вводим коэффициенты
-a, b, c = int(input('введите a: ')), int(input('введите b: ')), int(input('введите c: '))
+# min, max, average, summ, production
+N = 5
+total = 0
+prod = 1
+min_val = float('inf') # + бесконечность
+max_val = float('-inf') # - бесконечность
+average = 0
 
-if a != 0:
-    # Дискриминант
-    d = b ** 2 - 4 * a * c
+for _ in range(N):
+    num = int(input('Введите целое число: '))
+    if num < min_val:
+        min_val = num
+    if num > max_val:
+        max_val = num
+    total += num
+    prod *= num
+    average = total / N
 
-    # Вычисление корней
-    if d < 0:
-        print('Уравнение не имеет корней!')
-    elif d == 0:
-        print(f'Корень уравнения: {round(-b / (2 * a))}')
-    else:
-        print(f'Корни уравнения:\n\t\tx1 = {round((-b + d ** 0.5) / 2 * a)} \n\t\tx2 = {round((-b - d ** 0.5) / 2 * a)}')
-else:
-    print('По условию a не равно нулю! Начните программу заново!!!')
+print(f'Сумма: {total}')
+print(f'Произведение: {prod}')
+print(f'Ср арифметическое: {average}')
+print(f'Минимальное число: {min_val}')
+print(f'Максимальное число: {max_val}')
+
+# факториал
+N = 5
+fact = 1
+
+for i in range(1,N+1):
+    fact *= i
+
+print(fact)
