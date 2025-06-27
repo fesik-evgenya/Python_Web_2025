56116c879b605a414f3ff792d67b781acd503cae
Фесик учится дома <ganef85@mail.ru>
2025-06-27 22:31:52 +0300

    Set Operations

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
-# Города - проверка был ли город уже назван
+# Операции над множествами
+a = {3, 5, 7, 8}
+b = {3, 5, 7, 9, 11}
 
-s = set()
+# Объединение множеств
+c = a.union(b)  # c = a / b (идентичная запись)
+print(c)
 
-while (city := input('Назовите город: ')):
-    if city in s:
-        print('Город уже был!')
+# Пересечение множеств
+c = a.intersection(b)  # c = a & b (идентичная запись)
+print(c)
+
+# Разность множеств - есть в первом, но нет во втором
+c = a.difference(b)  # c = a - b (идентичная запись)
+print(c)
+
+# Симметричная разность множеств - вся разница от каждого множества вместе
+c = a.symmetric_difference(b)  # c = a ^ b (идентичная запись)
+print(c)
+
+# Удаление всех карт, кроме туза = Пример
+cards = {3, 7, 'T', 'D', 'V', 'K'}
+ace = {'T'}
+
+result = cards - ace
+print(result)
+
+# 2 вариант
+cards = {3, 7, 'T', 'D', 'V', 'K'}
+t_is = False
+
+while cards:
+    card = cards.pop()
+    if card == 'T':
+        cards.add(card)
+        t_is = True
     else:
-        s.add(city)
+        print(cards)
 
-print(f'Итого было названо: {len(s)} городов(а):')
-for item in s:
-    print('\t', item)
+    if t_is and len(cards) == 1:
+        break
