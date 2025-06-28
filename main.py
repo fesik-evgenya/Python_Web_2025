58bd535481b7d3c77a68d7fc9e46d6140b13be00
Фесик учится дома <ganef85@mail.ru>
2025-06-28 11:36:09 +0300

    String Intro + Vowers

--- main.py
+++ main.py
@@ -1,41 +1,28 @@
-# Коллекции = (set, list, dict, tuple)
-# Множества - Set ()
-# Может хранить разные типы данных
-# Неупорядоченная коллекция данных
-# Множество убирает повторы автоматом
+# Строки (immutable, iterable)
+# итерируемый, но не изменяемый объект
 
-s = set()  # пустое множество
-print(dir(s))  # - список методов множества
-
-s = {'3', '5', '7', '3', '3'}
-
-s.add('8.5')  # добавление нового элемента
-
-s.remove('3')  # удаление элемента -> вызывает ошибку, если элемента нет в множестве
-s.discard('3')  # удаление элемента в слепую
-temp = s.pop()  # удаление случайного элемента с возвратом значения
-print(temp)
-
-print(type(s)) # узнать класс объекта
-
-print(f'Число элементов в s = {len(s)}')
-
-print('Присутствует ли 3')
-if str(3) in s:
-    print('Да')
-else:
-    print('Нет')
-
-for item in s:
-    print(item)
+s = ''
+print(id(s))  # узнать адрес в памяти
+s += 'Привет'
+print(id(s))
 print(s)
 
-s.clear()  # удаление всех элементов из множества
 
-# 'add', 'clear', 'copy', 'difference',
-# 'difference_update', 'discard',
-# 'intersection', 'intersection_update',
-# 'isdisjoint', 'issubset', 'issuperset',
-# 'pop', 'remove', 'symmetric_difference',
-# 'symmetric_difference_update', 'union',
-# 'update'
+s = 'Python'
+# s[3] = 'y' error / не позволяет менять символы в строке (immutable)
+print(s[-1])  # выведет последний символ
+print(s[0])  # выведет первый символ
+print(f'Длина слова: {len(s)}')
+
+
+main_str = 'язык питон'
+
+total_Vw_Str = 0
+for s in main_str:
+    if s in {'а', 'о', 'я', 'и', 'ю', 'э', 'ы', 'е', 'у'}:
+        total_Vw_Str += 1
+print(f'В строке всего {total_Vw_Str} гласных(е) буквы')
+
+# Перебор строки по числовому индексу
+for index in range(len(main_str)):
+    print(main_str[index])
