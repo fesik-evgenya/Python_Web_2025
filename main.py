719ef3fedda39f3920001f7ef9c8272397648de7
Фесик на учёбе <ganef85@mail.ru>
2025-06-30 14:58:33 +0300

    List Intro + Metods + exss

--- main.py
+++ main.py
@@ -1,81 +1,100 @@
-# Методы строк - часть 2
-# Начало т окончание строки
-# startswith и endswith
+# # Списки (list) - изменяемая последовательность данных
+## может состоять из смешанных типов данных
+#
+# s = {'3', '4', '5'}
+# lst = list(s)
+# lst = list('Python')
+# lst = list(range(1,11))
+# lst = []  # пустой список
+# lst = [1, 2, 3]
+# lst = [1, 2, 3] * 3  # по аналогии со строками
+#
+# print(lst)
+#
+# # 'append', 'clear', 'copy', 'count', 'extend',
+# # 'index', 'insert', 'pop', 'remove', 'reverse', 'sort'
+#
+# s = 'сабака'
+# lst = list(s)
+#
+# lst[1] = 'о'
+# print(lst)
+#
+## append() - добавляет в конец списка значение
+# s = []
+# for i in range(11):
+#     s.append(i)
+#
+# print(s)
+#
+## extend() - расширяет список на значение ( например другой список
+# s1 = [1, 2, 3]
+# s2 = [4, 5, 6]
+#
+# s3 = s1.extend(s2).copy()
+#
+# print(s1.extend(s2))  # или s1 + s2
+# print(s3)
+#
+# lst = list(range(10))
+# sls = lst[2::2]
+#
+# print(sls)
+#
+# for i in range(0, len(lst), 2):
+#     print(lst[i], '-', lst[i] ** 2)
+#
+# del lst[2]  # удалить элемент(грубо)
+# remove() - удаляет первое значение в списке
 
+# pop() - забирает из списка по индексу значение, и возвращает его же.
+# по дефолту последний индекс списка
 
-s = 'Cмотреть'
+# sort() - сортировка списка. по умолчанию reverse = False
+# если нужно отсортировать от большего к меньшему - reverse = True.
 
-if s.lower().startswith('смо'):
-    print('Да')
+# a = ['a', 'b', 'c']
+# b = a  # не копируем, а всего лишь делаем ссылку на изначальную область памяти
+# b.append('d')
+#
+# print(id(a))
+# print(id(b))
+# print(a)
+# print(a)
+#
+# b = a[:]  # a.copy() - создаём новую ячейку памяти с данными
+#
+# lst_for_okroshka = []  # пустой список
+# # набираем ингредиенты
+# while (item := input('Какой ингредиент добавляем?: ')) != '':
+#     lst_for_okroshka.append(item)
+# # переводом в множество и обратно убираем дубляжи ввода
+# temp = set(lst_for_okroshka)
+# lst_for_okroshka = list(temp)
+#
+# print (f'\n\tУ нас есть {len(lst_for_okroshka)} ингредиента(ов): ')
+# # сортируем по алфавиту
+# lst_for_okroshka.sort()
+# # распечатываем список ингредиентов
+# for i in range(len(lst_for_okroshka)):
+#     print(f'\t{i+1}. {lst_for_okroshka[i]}')
+#
+# # реализовываем стек по книгам
+# lst_books = []
+#
+# while (book := input('Положите книгу в стопку: ')) != '':
+#     lst_books.append(book)
+#
+#
+# while (answer := input('Хотите забрать книгу? (нет/да): ')) == 'да' or not lst_books:
+#     print(f'Вы взяли книгу: {lst_books.pop()}')
+#
+# распаковка списка через * в print()
+lst_words = []
+result_str = ''
 
-if s.endswith('еть'):
-    print('Да')
+while (word := input('Введите слово: ')).strip() != '':
+     lst_words.append(word[0].upper())
 
-# find('подстрока', 'start', 'end')
-# возвращает первое вхождение искомой подстроки
-# возвращает (-1) - значит не найдено
-
-s = 'Смотреть, вертеть, видеть'
-
-index = s.find('еть')  # с начала строки
-print(index)
-
-index = s.find('еть', 10)  # с позиции старт
-print(index)
-index = s.find('еть')
-print(index)
-
-s = 'синхрофазотрон'  # ищем 'о': сколько их и где находятся
-ch = 'о'
-
-if ch in s:
-    count = s.count(ch)
-    print(f'Буква \'{ch}\' встречается в слове "{s}" {count} раз(а).')
-    print('Её позиция/позиции:', end=' ')
-    start = 0
-    for i in range(count):
-        pos = s.find(ch, start)
-        start = pos + 1
-        print(pos, end=' ')
-else:
-    print(f'Буквы \'{ch}\' нет в слове "{s}".')
-
-# replace ('что', 'на что', 'сколько раз')
-
-s = '+7-012-345-67-89'  # нужно заменить -012- на (012)
-
-print(s.replace('-', ' (', 1).replace('-', ') ', 1))
-
-# slice - срез ( у строки и у других коллекций, кроме set)
-# [начало:окончание:шаг]
-# обращение по среду намного быстрее других циклов
-# 1 и 0 можно не указывать
-
-s = 'добрый день'
-
-print(s[:6])  # от начала и до заданного индекса
-print(s[7:])  # от текущего индекса до конца
-print(s[3:8])  # от n до m (не включая)
-print(s[::-1])  # инверсия
-
-# проверка на палиндром
-s = input('Введите слово: ').strip()
-
-if s.lower() == s[::-1].lower():
-    print(f'Строка "{s}" является палиндромом')
-else:
-    print(f'Строка "{s}" не является палиндромом')
-
-# + и *  Миргород дорог... дорог...
-s = 'Дорог Рим город или дорог Миргород'
-new_symbols = '...'
-
-print(s[26:] + (s[19:25] + new_symbols) * 2)
-
-s = 'Дорог Рим'
-
-temp = s.lower()
-city = s[:5][::-1]
-res = city + ' ' + temp[6:][::-1] + city
-
-print(res.title())
+print('Получилась аббревиатура: ', end=': ')
+print(*lst_words, sep='')
