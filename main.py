2ebb5b22f748aaf10ee36adb17c688065c19f5a5
Фесик на учёбе <ganef85@mail.ru>
2025-06-30 16:17:34 +0300

    Tuple Intro + Unpack & Pack + sorted() % enumerate() Func

--- main.py
+++ main.py
@@ -1,101 +1,61 @@
-# Списки (list) - изменяемая последовательность данных
-# может состоять из смешанных типов данных
-
-s = {'3', '4', '5'}
-lst = list(s)
-lst = list('Python')
-lst = list(range(1,11))
-lst = []  # пустой список
-lst = [1, 2, 3]
-lst = [1, 2, 3] * 3  # по аналогии со строками
-
-print(lst)
-
-# 'append', 'clear', 'copy', 'count', 'extend',
-# 'index', 'insert', 'pop', 'remove', 'reverse', 'sort'
-
-s = 'сабака'
-lst = list(s)
-
-lst[1] = 'о'
-print(lst)
-
-# append() - добавляет в конец списка значение
-s = []
-for i in range(11):
-    s.append(i)
-
-print(s)
-
-# extend() - расширяет список на значение ( например другой список
-s1 = [1, 2, 3]
-s2 = [4, 5, 6]
-
-s3 = s1.extend(s2).copy()
-
-print(s1.extend(s2))  # или s1 + s2
-print(s3)
-
-lst = list(range(10))
-sls = lst[2::2]
-
-print(sls)
-
-for i in range(0, len(lst), 2):
-    print(lst[i], '-', lst[i] ** 2)
-
-del lst[2]  # удалить элемент(грубо)
-
-# remove() - удаляет первое значение в списке
+# # Кортеж ( tuple, immutable ) - такой же список, но не изменяемый
+# # в памяти занимает меньше места и обращение к нему быстрее
+# BLACK = (0, 0, 0)  # константа
 #
-# pop() - забирает из списка по индексу значение, и возвращает его же.
-# по дефолту последний индекс списка
+# empty = () #tuple()
+# one = (1,)  #tuple()
+# temper = 36,6  #tuple()
+# s = 'Python'
+# t = tuple(s) + ('.',)
+# print(t)
 #
-# sort() - сортировка списка. по умолчанию reverse = False
-# если нужно отсортировать от большего к меньшему - reverse = True.
+# # 'count', 'index'
+#
+# cards = [(7, 'червей'),('туз', 'пик'),('дама', 'бубей')] # список кортежей
+#
+# print((1, 2) <= (2, 1))  # сравнение кортежей
+#
+## распаковка списков с помощью кортежей (работает со строками и множествами)
+## !!! количество переменных должно быть равно кол-ву элементов
+#
+# channels = ['red', 'green', 'blue']
+#
+# r, g, b = channels  # распаковка
+#
+# print(r)
+#
+# r, *other = channels # частичная распаковка
+#
+# print(other)
+#
+# first, second = input(), input()
+# print(first, second)
+#
+# # список студентов со средними баллами
+# N = 3
+# lst_students = []
+#
+# for _ in range(N):
+#     person , avr_score = input('Фамилия: '), float(input('средний балл: '))
+#     lst_students.append((person, avr_score))
+#
+# print(lst_students)
+#
+# for st in lst_students:
+#     person, avr_score = st
+#     print('Студент: ', person)
+#     print('Средний балл', avr_score)
+#
+# # функция sorted() - что бы не подали ей на вход, она вернёт отсортированный список
+#
+# s = {'Петров', 'Иванов', 'Сидорова'}
+#
+# lst = sorted(s, reverse=True)
+# print(*lst, sep=', ')
 
-a = ['a', 'b', 'c']
-b = a  # не копируем, а всего лишь делаем ссылку на изначальную область памяти
-b.append('d')
+# функция enumerate возвращает в цикле 'for' пару(индекс и значение) в кортеже
 
-print(id(a))
-print(id(b))
-print(a)
-print(a)
+fio = ['Сидорова', 'Петров', 'Иванов']
 
-b = a[:]  # a.copy() - создаём новую ячейку памяти с данными
-
-lst_for_okroshka = []  # пустой список
-# набираем ингредиенты
-while (item := input('Какой ингредиент добавляем?: ')) != '':
-    lst_for_okroshka.append(item)
-# переводом в множество и обратно убираем дубляжи ввода
-temp = set(lst_for_okroshka)
-lst_for_okroshka = list(temp)
-
-print (f'\n\tУ нас есть {len(lst_for_okroshka)} ингредиента(ов): ')
-# сортируем по алфавиту
-lst_for_okroshka.sort()
-# распечатываем список ингредиентов
-for i in range(len(lst_for_okroshka)):
-    print(f'\t{i+1}. {lst_for_okroshka[i]}')
-
-# реализовываем стек по книгам
-lst_books = []
-
-while (book := input('Положите книгу в стопку: ')) != '':
-    lst_books.append(book)
-
-
-while (answer := input('Хотите забрать книгу? (нет/да): ')) == 'да' or not lst_books:
-    print(f'Вы взяли книгу: {lst_books.pop()}')
-
-# распаковка списка через * в print()
-lst_words = []
-result_str = ''
-
-while (word := input('Введите слово: ')).strip() != '':
-     lst_words.append(word[0].upper())
-
-print('Получилась аббревиатура: ', end=': ')
-print(*lst_words, sep='')
+for i, v in enumerate(fio):
+    print(f'{i + 1}.{v}.')
