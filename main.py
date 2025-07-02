f3282a9bafb3fe0c89be22970241c9a64fb67070
Фесик на учёбе <ganef85@mail.ru>
2025-07-02 11:33:25 +0300

    Not TODO + Local, Global and main()

--- main.py
+++ main.py
@@ -1,15 +1,67 @@
-print('''Витязь на распутье:
-         Налево (L) пойдёшь, вольну-волю обретёшь...
-         Направо (R) пойдёшь, коня потеряешь...
-         Прямо (F) пойдёшь, сыт ип весел будешь...''')
+# # Global and Local ( область видимости)
+# # Пример как делать не надо
+# a = [1,2]
+#
+#
+# def changes_array():
+#     a[0] = 0
+#
+# changes_array()
+# print(a)
 
-choice = input('Куда идём (L,R или F): ')
 
-if choice == 'L' or choice == 'l':
-    print('вольну-волю обретаешь, йоху!!!')
-elif choice == 'R':
-    print('коня потерял (((')
-elif choice == 'F':
-    print('сыт и весел... Ура!!!')
-else:
-    print('<<странный выбор -_-, очень>>')
+PI = 3.14
+
+def print_array(array: list) -> None:
+    for item in array:
+        print(item)
+
+
+def circle_length(radius: float) -> None:
+    """
+    расчёт длины окружности по радиусу
+    :param radius: радиус окружности
+    :return: длина окружности
+    """
+    perimetr = 2 * PI * radius
+    print(f'Длина окружности с радиусом <{radius}> = {perimetr:.2f} ')
+
+
+def square_area(length: int, width: int) -> None:
+    """
+    расчёт площади
+    :param length: длина
+    :param width: ширина
+    :return: ничего не возвращает
+    """
+    area = length * width
+    print(f'Площадь = {area}')
+
+
+def greet(name: str) -> None:
+    """
+    Greeting
+    :param name: Name
+    :return: None
+    """
+    print('Привет', name)
+    name = 'друг'  # переназначаем переменную!!!
+    print('Здравствуй', name)
+
+
+def main():
+    square = 'Дворцовая площадь'
+    print('Давай встретимся, где', square)
+    square_area(320, 240)
+    print('Ну что? Встречаемся, где', square)
+
+    name = 'Пётр'
+    greet(name)
+
+    circle_length(5)
+
+    words = ['Привет', 'мир']
+    print_array(words)
+
+
+main()
