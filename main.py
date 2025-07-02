8509203ed9291202937325c8cb2e065a950625c0
Фесик на учёбе <ganef85@mail.ru>
2025-07-02 11:57:25 +0300

    return vs yield

--- main.py
+++ main.py
@@ -1,67 +1,11 @@
-# # Global and Local ( область видимости)
-# # Пример как делать не надо
-# a = [1,2]
-#
-#
-# def changes_array():
-#     a[0] = 0
-#
-# changes_array()
-# print(a)
+# return vs yield
+# разница <yield> - функция возвращает значение и продолжает работать
+# создаёт генератор
+
+def generate_list():
+    for i in range(5):
+        yield i  # генератор (возвращает, но не завершает)
 
 
-PI = 3.14
-
-def print_array(array: list) -> None:
-    for item in array:
-        print(item)
-
-
-def circle_length(radius: float) -> None:
-    """
-    расчёт длины окружности по радиусу
-    :param radius: радиус окружности
-    :return: длина окружности
-    """
-    perimetr = 2 * PI * radius
-    print(f'Длина окружности с радиусом <{radius}> = {perimetr:.2f} ')
-
-
-def square_area(length: int, width: int) -> None:
-    """
-    расчёт площади
-    :param length: длина
-    :param width: ширина
-    :return: ничего не возвращает
-    """
-    area = length * width
-    print(f'Площадь = {area}')
-
-
-def greet(name: str) -> None:
-    """
-    Greeting
-    :param name: Name
-    :return: None
-    """
-    print('Привет', name)
-    name = 'друг'  # переназначаем переменную!!!
-    print('Здравствуй', name)
-
-
-def main():
-    square = 'Дворцовая площадь'
-    print('Давай встретимся, где', square)
-    square_area(320, 240)
-    print('Ну что? Встречаемся, где', square)
-
-    name = 'Пётр'
-    greet(name)
-
-    circle_length(5)
-
-    words = ['Привет', 'мир']
-    print_array(words)
-
-
-main()
+array = list(generate_list())
+print(array)
