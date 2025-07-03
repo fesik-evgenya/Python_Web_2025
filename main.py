b26f349020775587b20170d0eaca0d19f9159ce2
Фесик на учёбе <ganef85@mail.ru>
2025-07-03 10:12:54 +0300

    Lesson6 - Home Work

--- main.py
+++ main.py
@@ -1,35 +1,26 @@
-# Функция как объект
-# передаётся в другие функции, и эти функции называются -> Функция высшего порядка
-# функция критерия отбора элементов списка
-# Критерий: длина слова
-def is_longer_six(word):
-    return len(word) > 6
+# превратить список num в строку цифр без пробелов(и распечатать) с помощью map()
 
-words = ['В', 'этом', 'списке', 'останутся', 'слова',
-         'длина', 'которых', 'больше', 'шести']
-
-for word in filter(is_longer_six, words):
-    print(word)
-
-result = list(filter(is_longer_six, words))
-print(result)
-
-fruits = ['Слива', 'Ананас', 'Апельсин', 'Малина', 'Яблоко',
-         'Дыня', 'Смородина', 'Манго', 'Банан', 'Ежевика']
-
-def find_firs_let_A(fruit: str):
-    return fruit[0] == 'А'
-
-#map
-for fruit in filter(find_firs_let_A, fruits):
-    print(fruit)
-
-def square(num):
-    return num ** 2
+def square(num: int) -> str:
+    """
+    возводит в квадрат и переводит в строку
+    :param num: число
+    :return: число в квадрате типа строка
+    """
+    return str(num ** 2)
 
 
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 squares = map(square, nums)
-print(list(squares))
 
-# превратить список num в строку цифр без пробелов (и распечатать) с помощью map()
+print(*list(squares), sep='')
+
+# Критерий - вхождение подстроки
+# в частности 'ан'
+fruits = ['Слива', 'Ананас', 'Апельсин', 'Малина', 'Яблоко',
+         'Дыня', 'Смородина', 'Манго', 'Банан', 'Ежевика']
+
+def string_contain(s):
+    return 'ан' in s
+
+res = list(filter(string_contain, fruits))
+print(res)
