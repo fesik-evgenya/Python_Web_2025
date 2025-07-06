adab93617f2c61c93988ee7c0b501b2fabc4526f
Фесик учится дома <ganef85@mail.ru>
2025-07-06 12:38:36 +0300

    Lesson8 - Home Work (has done)

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
-# Внешние библиотеки
-# Графика
-# PIL - Python Image Library
-# RGB
-# thumbnail
 from PIL import Image, ImageDraw
-
-RED = (255, 0, 0)
-WHITE = (255, 255, 255)
-POLY = [(50, 50), (150,50), (150,150)]
-
-image = Image.new('RGB', (600, 400), (0, 0, 255))
-image.save('./images/blue.jpg')
+# объявляем константы цветов
+BLUE = (125, 190, 255)
+YELLOW = (255, 242, 0)
+# создаём холст
+image = Image.new('RGB', (600, 400), BLUE)
 
 draw = ImageDraw.Draw(image)
-draw.line((0, 0, 600, 400),fill=RED, width=5)
-draw.line((600, 0, 0, 400),fill=RED, width=5)
-draw.rectangle((10, 10, 590, 390), outline=WHITE)
-draw.ellipse((250, 150, 350, 250), fill=WHITE)
-
-draw.polygon(POLY, outline='green', width=5)
-draw.text((150, 50), 'Текст', fill=WHITE, font_size=45)
-
-image.save('./images/blue.jpg')
+# рисуем солнце и пишем заголовок
+draw.ellipse((500, -100, 700, 100), fill=YELLOW)
+draw.text((170, 160), 'SUNNY DAY', fill=YELLOW, font_size=45)
+# сохраняем изображение
+image.save('./images/sunny_day.jpg')
