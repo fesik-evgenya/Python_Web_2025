b549ea24e1f258602250c64fbb1e125c102baedf
Фесик на учёбе <ganef85@mail.ru>
2025-07-07 10:19:05 +0300

    Using Custom Fonts

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
-print('''Витязь на распутье:
-         Налево (L) пойдёшь, вольну-волю обретёшь...
-         Направо (R) пойдёшь, коня потеряешь...
-         Прямо (F) пойдёшь, сыт ип весел будешь...''')
+from PIL import Image, ImageDraw, ImageFont
 
-choice = input('Куда идём (L,R или F): ')
+# https://fontsforyou.com/ru/specific-fonts/ttf-fonts/languageru
+W = 600
+H = 400
 
-if choice == 'L' or choice == 'l':
-    print('вольну-волю обретаешь, йоху!!!')
-elif choice == 'R':
-    print('коня потерял (((')
-elif choice == 'F':
-    print('сыт и весел... Ура!!!')
-else:
-    print('<<странный выбор -_-, очень>>')
+image = Image.new('RGB',
+                  (W, H),
+                  (0, 163, 232))
+
+draw = ImageDraw.Draw(image)
+
+text = 'Солнечный день'
+# draw.ellipse((470, -120, 800, 120), outline='yellow', fill='yellow')
+draw.circle((600, 0), 100, fill='yellow')
+font = ImageFont.truetype(
+    font='./fonts/main_font.ttf',  # можно использовать любой установленный шрифт
+    size=40
+)
+# Получаем размеры текста с помощью распаковки
+_, _, w, h = draw.textbbox((0, 0), text, font=font)
+
+# Рассчитываем позицию для центрирования
+x = (W - w) // 2
+y = (H - h) // 2
+
+draw.text((x, y), text, fill=(255, 255, 0), font=font)
+
+image.save('images/sunny_day_2.jpg')  # image.show() - показать картинку без сохранения
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
