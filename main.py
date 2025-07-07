d56aa11d6e33b6ec911c06cf3817951df20c8a0e
Фесик на учёбе <ganef85@mail.ru>
2025-07-07 11:33:32 +0300

    Docx Creation + Docx Template (fixed)

--- main.py
+++ main.py
@@ -1,45 +1,45 @@
-## Внешние библиотеки
-## Документы
-## Word - DOCX (python - DOCX)
-# from docx import Document
-# from docx.enum.text import WD_ALIGN_PARAGRAPH
-# # для размеров
-# from docx.shared import  Mm   # Inches(дюймы) Mm(миллиметры) Pt(Поинт - 1/72 дюйма)
-#
-# doc = Document()  # создание экземпляра документа
-# # Добавление заголовка
-# heading = doc.add_heading('Отчёт за месяц', 1)
-# heading.alignment= WD_ALIGN_PARAGRAPH.CENTER
-#
-# paragraph = doc.add_paragraph('В этом отчёте представлены')
-# paragraph.alignment= WD_ALIGN_PARAGRAPH.CENTER
-# # run - что-то внутри абзаца ( текст, картинка и т.п.)
-# paragraph.add_run(' ключевые показатели').bold = True
-#
-# # Новый параграф для списка
-# paragraph = doc.add_paragraph()  # вставляем пустую строку
-# paragraph_format = paragraph.paragraph_format
-# paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
-#
-# # маркированный список
-# paragraph = doc.add_paragraph('Первый пункт', style='List Bullet')
-# paragraph = doc.add_paragraph('Второй пункт', style='List Bullet')
-#
-# # нумерованный список
-# paragraph = doc.add_paragraph('Первый пункт', style='List Number')
-# paragraph = doc.add_paragraph('Второй пункт', style='List Number')
-#
-# # добавляем таблицу
-# table = doc.add_table(rows=3, cols=3)
-# # Заполняем таблицу
-# for i, row in enumerate(table.rows):
-#     for j, cell in enumerate(table.columns):
-#         cell.text = f'Строка {i+1}, Столбец{j+1}'
-#
-# doc.add_paragraph()
-# doc.add_picture('./images/sunny_day_2.jpg', width=Mm(105))
-#
-# doc.save('./docs/report.docx')
+# Внешние библиотеки
+# Документы
+# Word - DOCX (python - DOCX)
+from docx import Document
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+# для размеров
+from docx.shared import  Mm   # Inches(дюймы) Mm(миллиметры) Pt(Поинт - 1/72 дюйма)
+
+doc = Document()  # создание экземпляра документа
+# Добавление заголовка
+heading = doc.add_heading('Отчёт за месяц', 1)
+heading.alignment= WD_ALIGN_PARAGRAPH.CENTER
+
+paragraph = doc.add_paragraph('В этом отчёте представлены')
+paragraph.alignment= WD_ALIGN_PARAGRAPH.CENTER
+# run - что-то внутри абзаца ( текст, картинка и т.п.)
+paragraph.add_run(' ключевые показатели').bold = True
+
+# Новый параграф для списка
+paragraph = doc.add_paragraph()  # вставляем пустую строку
+paragraph_format = paragraph.paragraph_format
+paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
+
+# маркированный список
+paragraph = doc.add_paragraph('Первый пункт', style='List Bullet')
+paragraph = doc.add_paragraph('Второй пункт', style='List Bullet')
+
+# нумерованный список
+paragraph = doc.add_paragraph('Первый пункт', style='List Number')
+paragraph = doc.add_paragraph('Второй пункт', style='List Number')
+
+# добавляем таблицу
+table = doc.add_table(rows=3, cols=3)
+# Заполняем таблицу
+for i, row in enumerate(table.rows):
+    for j, cell in enumerate(table.columns):
+        cell.text = f'Строка {i+1}, Столбец{j+1}'
+
+doc.add_paragraph()
+doc.add_picture('./images/sunny_day_2.jpg', width=Mm(105))
+
+doc.save('./docs/report.docx')
 
 # Word - DOCX (docxtpl)
 from docxtpl import DocxTemplate
