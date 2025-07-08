0e3ef10a3e74822736c1a41f6a4bb016fcf0ba9f
Фесик на учёбе <ganef85@mail.ru>
2025-07-08 14:40:37 +0300

    Tasks - selfeducation Dict

--- main.py
+++ main.py
@@ -1,139 +1,53 @@
-# Исключение - аварийная остановка программы (runtime)
-# try:
-#     что пытаемся сделать
-# except:
-#     обрабатываем исключения
-# finally:
-#     выполняется в любом случае
-# <NameError> - исключение, которое связано с необъявленной переменной
+# Практикум (обучаемый словарь)
+import pickle
+from fnmatch import translate
 
-flag = False  # открывался ли на запись файл
+# минимальная версия, если файл dick.dat отсутствует
+voc = {
+    'стол': 'table',
+    'стул': 'chair',
+}
 
+
+# функция для распечатки словаря
+def print_voc():
+    print('Сейчас словарь содержит: ')
+    for k, v in voc.items():
+        print(k, '—', v)
+
+# загружаем словарь из файла
 try:
-    fo = open('information.txt', encoding='utf-8')
-
+    with open('dict.dat', 'rb') as dump_in:
+        voc = pickle.load(dump_in)
 except FileNotFoundError:
-    fo = open('information.txt', 'wt', encoding='utf-8')
-    flag = True
-    print('Фал не обнаружен и создан с параметрами по умолчаниями')
-    # with open('information.txt', 'wt', encoding='utf-8') as fo:
-    #     fo.write('по умолчанию')
+    with open('dict.dat', 'wb') as dump_out:
+        pickle.dump(voc, dump_out)
+    print('Создан минимальный словарь: ')
+    print_voc()
 
-else:
-    print('Файл открыт успешно. Читаем его и закрываем.')
-    print(fo.read())
-    fo.close()
-
-finally:
-    if flag: # если было исключение и фал был открыт для записи
-        fo.write('по умолчанию')
-        fo.close()
-    print('Продолжаем работать.')
-
-print('Остаток от деления: ')
-
-try:
-    value = int(input('На что делим число 10: '))
-    res = 10 % value
-    print(f'Остаток от деления 10 на {value} = {res}')
-
-# исключение деления на ноль
-except ZeroDivisionError:
-    print('На ноль делить нельзя!')
-
-except ValueError:
-    print('Надо вводить только целые числа!')
-
-# вернуть инфу что за исключение произошло
-except Exception as exp:
-    print('Произошло исключение: ', exp.__class__.__name__,'=', exp)
-
-# тоже в цикле - пока пользователь не введёт правильную инфу
-print('Остаток от деления: ')
-flag = True
-
-while flag:
-    try:
-        value = int(input('На что делим число 10: '))
-        res = 10 % value
-        print(f'Остаток от деления 10 на {value} = {res}')
-
-    except ZeroDivisionError:
-        print('На ноль делить нельзя!')
-
-    except ValueError:
-        print('Надо вводить только целые числа!')
-
-    except Exception as exp:
-        print('Произошло исключение: ', exp.__class__.__name__,'=', exp)
-
-    else:
-        flag = False
-
-# "Бросаемся" исключениями - raise
-max_val = 10
-min_val = 1
-
-try:
-    val = int(input(f'Введите целое число от {min_val} до {max_val}: '))
-    if not min_val < val < max_val:
-        raise ValueError('Введённое число вне диапазона')
-    print(f'Введённое число {val} лежит в заданном диапазоне')
-except ValueError as exp:
-    print('Надо быть внимательнее:', exp)
-
-# Утверждения (assertions) - в продакшине не используется, только в тестировании
-try:
-    text = input('Введите текст: ')
-    assert len(text) > 3  # это утверждение
-except AssertionError:
-    print('Слишком короткий текст')
-
-# Задача 1.
-lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-loop = True
-
-while loop:
-    try:
-        ind = int(input(f'Ведите индекс(целое число) в диапазоне от 0 до {len(lst)-1}: '))
-        print(f'Число по индексу [{ind}]: {lst[ind]}')
-
-    except IndexError:
-        print(f'Индекс должен быть до {len(lst)-1}.')
-
-    except ValueError:
-        print(f'Индекс должен быть целым числом!')
-
-    except KeyboardInterrupt:
-        print(f'Программа остановлена')
+while True:
+    temp = input('\nВведите слово для перевода или "#" для завершения: ')
+    word = temp.strip().lower()
+    if word == '#' or word == '№':
         break
-
-    except Exception as exp:
-        print('Произошло исключение: ', exp.__class__.__name__,'=', exp)
-
+    if word in voc:
+        translate = voc[word]
+        print(f'Слово "{word}" переводится как {translate}.\n')
     else:
-        loop = False
+        print(f'Значение слова "{word}" отсутствует в словаре.')
+        newkey = f'А как слово "{word}" переводится. '
+        newkey += 'Если ничего не вводите нажмите ENTER,\n '
+        newkey += 'или введите его здесь: '
+        new_word = input(newkey)
 
-# Задача 2.
-loop = True
+        if new_word != '' or len(new_word) > 2:
+            voc[word] = new_word
+            print(f'Слово "{word}" с переводом "{new_word}" внесено в словарь')
+        else:
+            print('Ничего не введено или слишком короткое слово')
+            continue
+# сохранить словарь
+with open('dict.dat', 'wb') as dump_out:
+    pickle.dump(voc, dump_out)
 
-while loop:
-    try:
-        a = int(input('Введите число <a>: '))
-        b = int(input('Введите число <b>: '))
-        print(f'Итог деления {a} на {b}: {a / b}')
-
-    except ZeroDivisionError:
-        print('На ноль делить нельзя!')
-
-    except ValueError:
-        print('Необходимо ввести число!')
-
-    except Exception as exp:
-        print('Произошло исключение: ', exp.__class__.__name__, '=', exp)
-
-    except KeyboardInterrupt:
-        print('Выполнение программы прервано. Запустите программу снова.')
-        break
-    else:
-        loop = False
+print('До новых встреч!!!')
