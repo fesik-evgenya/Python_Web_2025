237a3353f15eec7b514bcbe64349825902a0a4f2
Фесик на учёбе <ganef85@mail.ru>
2025-07-09 16:00:45 +0300

    ООП (OOP) Intro + Getter_&_Setter + Static Members + practice

---
+++ lib.py
@@ -0,0 +1,124 @@
+class Sorted:
+    def __init__(self):
+        self._words = []
+
+    def add_word(self, word: str):
+        pass
+
+    def result(self):
+        # список слов отсортированный по длине
+
+
+
+class Separator:
+    def __init__(self):
+        self._odd = []
+        self._even = []
+
+    def add_num(self, num):
+            self._even.append(num) if not num % 2 else self._odd.append(num)
+
+    def get_odd(self):
+        return self._odd
+
+    def get_even(self):
+        return self._even
+
+
+class Clicker:
+    def __init__(self):
+        self._counter = 0
+
+    def click(self):
+        self._counter += 1
+
+    def get_count(self):
+        return self._counter
+
+    def reset(self):
+        self._counter = 0
+
+
+class Car:
+    counter = 0  # статичное свойство (счетчик)
+    def __init__(self, brand='No Brand', model='No Model', color='black'):
+        self._brand = brand
+        self._model = model
+        self._color = color
+        self._engine_on = False
+        Car.counter += 1
+
+    def start_engine(self):
+        self._engine_on = True  # пока не сработает
+
+    def drive_to(self, place):
+        if self._engine_on:
+            print(f'Едем в {place} на {self._brand} {self._model} цвета {self._color}')
+        else:
+            print('Двигатель не заведён, не едем!')
+
+    # getters
+    def get_brand(self):
+        return self._brand
+
+    def get_model(self):
+        return self._model
+
+    def get_color(self):
+        return self._color
+
+    # setters
+    def set_brand(self, new_brand):
+        if new_brand:
+            self._brand = new_brand
+
+    def set_model(self, new_model):
+        if new_model:
+            self._model = new_model
+
+    def set_color(self, new_color):
+        if new_color:
+            self._color = new_color
+
+    @staticmethod
+    def get_counter():
+        return Car.counter
+
+
+class Person:
+    def __init__(self, name='Bill', age=1):
+        self._name = name
+        self._age = age
+
+    # setters
+    def set_name(self, new_name):
+        if new_name:
+            self._name = new_name
+
+    def set_age(self,new_age):
+        if 0 < new_age < 150:
+            self._age = new_age
+        else:
+            print('Некорректный возраст — ', new_age)
+
+    # getters
+    def get_name(self):
+        return self._name
+
+    def get_age(self):
+        return self._age
+
+    def person_info(self):
+        return f'Человек с именем {self._name}. Возраст: {self._age}'
+
+
+def summ(a, b):
+    return a + b
+
+
+def diff(a, b):
+    return a - b
+
+
+if __name__ == '__main__':
+    print('Это библиотека, а исполняемый - main.py')
--- main.py
+++ main.py
@@ -1,79 +1,101 @@
-# https://regex101.com
-import re
-# 2-я 'н' может присутствовать, но не обязательно
-pattern = r'стеклянн?ый'
-test_string = 'стеклянный, стекляный, оловянный, серебрянный'
+# ООП - объектно ориентированное программирование
+# (encapsulation) - каждый объект это какая-то капсула.
+# Применяется для безопасности.
+# Класс - это прототип будущего объекта, который описывает его свойства и поведение,
+# и это такой-то набор данных и методы по работе с ними.
+# Экземпляр - это объект, порождённый классом
+# Атрибуты - это всё что внутри класса (свойства и методы)
+# Метод - это действие объекта
+#
+# Свойства классов:
+#
+# a = None
+# print(a.__class__.__name__)
+#
+# # создали класс
+# class Fruit:
+#     pass
+#
+#
+# a = Fruit() # создали экземпляр класса
+#
+# print(a.__class__)
+#
+# a.name = 'Яблоко'
+# a.weight = 120
+#
+# print(a.name)
+# print(a.weight)
+#
+# b = Fruit()
+# b.name = 'Груша'
+# b.weight = 90
+#
+# print(b.name, b.weight)
+#
+# # методы классов
+# # в self - это контекстный объект,
+# # в который передаётся вызванный метод ->
+# # (-> это ссылка, которая ведёт на адрес в оперативной памяти)
+# class Greater:
+#     def hello(self, name='Noname') -> None:
+#         print('Привет,', name)
+#
+#     def goodbye(self, name='Noname') -> None:
+#         print('Пока-Пока!', name)
+#
+#
+# g1 = Greater()
+# g1.hello('Ольга')
+# g1.goodbye('Иван')
+#
+# g2 = Greater()
+# g2.hello('Иван')
+# g2.goodbye('Ольга')
 
-# жадный квантификатор (greedy quantifier)
-pattern = r'<img.*>'
-test_string = 'Картинка <img src="bg.jpg"> в тексте</p>'
+# Методы классов и анализ предыдущих вызовов
+# from lib import Person
+#
+# car = Car('Scoda', 'Oktavia', 'red')
+# car.start_engine()
+# car.drive_to('Город')
+#
+#
+# p = Person()
+# p.set_age(785)
+# print(p.get_age())
+# print(p.person_info())
+#
+# # Статичные члены класса
+from lib import Car
+#
+# car1 = Car()
+# car2 = Car()
+# car3 = Car()
+#
+# print(f'В парке машин: {Car.get_counter()} шт')
+#
+# car = Car('Scoda', 'Oktavia')
+# car.set_color('pink')
+# car.start_engine()
+# car.drive_to('Город')
+#
+# from lib import Clicker
+#
+# cl = Clicker()
+# cl.click()
+# cl.click()
+# cl.click()
+#
+# print(cl.get_count())
+# cl.reset()
+# print(cl.get_count())
+#
+# from lib import Separator
+#
+# nums = Separator()
+# for i in range(100):
+#     nums.add_num(i)
+# print(f'Список чётных чисел: {nums.get_even()}; Список нечётных чисел: {nums.get_odd()}')
 
-result = re.findall(pattern, test_string)
-print(result)
-
-# ленивый квантификатор (lazy, non-greedy quantifier)
-pattern = r'<img.*?>'
-test_string = 'Картинка <img src="bg.jpg"> в тексте</p>'
-
-result = re.findall(pattern, test_string)
-print(result)
-
-# только путь к картинке
-pattern = r'<img[^>]+src="([^">]+)"'
-test_string = 'Картинка <img src="bg.jpg"> в тексте</p>'
-
-result = re.findall(pattern, test_string)
-print(result)
-
-# Захватить содержимое тега <p>
-pattern = '<p>(.*?)</p>'
-test_string= '<b>вот начало: </b><p>Содержимое</p><i>и т.д.</i>'
-
-result = re.findall(pattern, test_string)
-print(result)
-
-# захват абзаца в теге с атрибутами
-pattern = r'<p[^>]*>(.*)</p>'
-test_string= '<b>Центрируем</b><p align="center">Содержимое</p>'
-result = re.findall(pattern, test_string)
-print(result)
-
-# Убираем все знаки препинания
-def remove_punctuation(input_str: str) -> str:
-    """
-    Методом sub() заменяем все найденные совпадения
-    пустой строкой и возвращаем "очищенную"
-    :param input_str: строка со знаками препинания
-    :return: строку, очищенную от зн. препинания
-    """
-    return re.sub(r'[^\w\s]', '', input_str)
-
-test_string = 'Язык Python, являясь интуитивно понятным, прост для изучения. Ну и PEP8.'
-print(remove_punctuation(test_string))
-
-# через split() и join()
-pattern = r'[,.;:! ]'
-# убираем все пробелы
-test_string = ''.join('яблоко,  груша,  банан;  слива  !  абрикос  '.split(' '))
-
-result = re.split(pattern, test_string)
-print(result)
-
-# через регулярное выражение
-pattern = r'[,.;:!]'
-test_string = '  яблоко,  груша,  банан;  слива  !  абрикос  '
-
-result = sorted(x.strip() for x in re.split(pattern, test_string))
-print(result)
-
-# вытащить инфу с сайта ( например ссылки на изображения)
-import requests
-pattern = r'<img[^>]+src="([^">]+)"'
-# сначала проверим
-# text_string = '<img height="50" width="150" src="./images/sunny_day_2.jpg"><>'
-
-html = requests.get('https://skillbox.ru').text
-result = re.findall(pattern, html)
-print(result)
-# result = re.findall(pattern, text_string)
-# print(result)
+s = Sorted()
